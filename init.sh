1d63992b391bf78b936d53d7b2ad41fb17a38633
Kent Anderson <kent@kornerstoane.com>
2014-05-22 22:36:26 -0700

    Better qa after init is done

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ mkdir bundle
 cd bundle
 git clone https://github.com/gmarik/Vundle.vim.git Vundle.vim
 cd ..
-vim -c "PluginInstall" -c "q" -c "q"
+vim -c "PluginInstall" -c "qa!" 
 
 echo Vim environment is initialized.  Enjoy!
